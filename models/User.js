7c26379a8bfb833e24be34c7c6abb10a8a00dbd7
Taulant Culi <t.culi91@gmail.com>
2020-09-28 17:00:37 +0200

    using User Model to passport file and save google profile id to db

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  id: String,
+  googleId: String,
   name: String,
   email: String,
 });
+
+mongoose.model("users", userSchema);
