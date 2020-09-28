7aa45c76d490c772407c88c4756fa0fcb19ed97f
Taulant Culi <t.culi91@gmail.com>
2020-09-28 15:54:20 +0200

    adding mongoosJs and connection to DB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+
+//Connection with the DB
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 const app = express();
 
