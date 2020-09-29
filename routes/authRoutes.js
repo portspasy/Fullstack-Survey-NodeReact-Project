3d8618009593659046eae95a298c8e15e267e1a2
Taulant Culi <t.culi91@gmail.com>
2020-09-29 16:25:00 +0200

    adding log out for user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,13 @@ module.exports = (app) => {
 
   // Google OAuth Callbacks
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
